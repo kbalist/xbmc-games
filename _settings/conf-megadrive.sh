3e42336fc38425bc04a3424c3c2b4403aff6ffa6
Kbalist <kbalist@gmail.com>
2011-07-27 19:47:42 +0200

    clean conf launcher

--- _settings/conf-megadrive.sh
+++ _settings/conf-megadrive.sh
@@ -1,32 +1,5 @@
 #!/usr/bin/env bash
 
-####################################
 # MEGADRIVE / GENESIS LAUNCHER
-####################################
-# Thanks to http://doc.ubuntu-fr.org/gens-gs
-
-# launcher : gens
-# manual : 
-# romext : zip|bin|cue|iso|32x
-
-# config : 
-# Prior to use Dolphin emulator with XBMC you need to use its GUI to modify some options. Into Options/Configure/Display menu:
-# 	- Check "Start Renderer in Fullscreen" option
-# 	- Check "Hide mouse cursor" option
-# 	- Uncheck "Confirm on Stop" option
-# 	- Into Options/Configure/Path menu select the directory where are located your iso files.
-#
-# For some Linux system configuration, the Dolphin emulator cannot be directly started from XBMC due to a problem related to users privilege access. This could be bypassed by stating dolphin-emu using the sudo command line as follow.
-# Application
-# 		/usr/bin/sudo
-# Arguments
-# 		-u username /usr/bin/dolphin-emu -e "%rom%" -b
-# Where username is the name of the user with the rights to execute dolphin-emu.
-
-# command line options :
-#	-e : rom to load
-#	-b : ??
-####################################
 
 /usr/bin/gens
-
--- _settings/conf-psx2.sh
+++ _settings/conf-psx2.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-####################################
 # PSX2 LAUNCHER
-####################################
-
-# launcher : pcsx2
-# manual : man pcsx2
-# romext : iso|bin
-
-# command line options :
-#	-nogui : Don't load the GUI
-#	-cdfile : run specified file (missing from man)
-####################################
 
 /usr/games/pcsx2
-
-
 
